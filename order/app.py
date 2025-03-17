525a3c66bbe4b0519113bf76a49bf4568c1b0d51
George Latsev <g.s.latsev@student.tudelft.nl>
2025-03-17 17:03:00 +0100

    impl: add async rabbitmq client

--- order/app.py
+++ order/app.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 import atexit
@@ -5,28 +6,30 @@ import random
 import uuid
 from collections import defaultdict
 
-import redis
+import redis.asyncio as redis
 import requests
 
 from msgspec import msgpack, Struct
-from flask import Flask, jsonify, abort, Response
+from quart import Quart, jsonify, abort, Response
 
+from rabbit_client import RabbitClient
 
 DB_ERROR_STR = "DB error"
 REQ_ERROR_STR = "Requests error"
 
 GATEWAY_URL = os.environ['GATEWAY_URL']
 
-app = Flask("order-service")
+app = Quart("order-service")
 
 db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
                               port=int(os.environ['REDIS_PORT']),
                               password=os.environ['REDIS_PASSWORD'],
                               db=int(os.environ['REDIS_DB']))
 
+rabbit_client = RabbitClient(os.environ['RABBIT_URL'])
 
-def close_db_connection():
-    db.close()
+async def close_db_connection():
+    await db.close()
 
 
 atexit.register(close_db_connection)
@@ -38,11 +41,10 @@ class OrderValue(Struct):
     user_id: str
     total_cost: int
 
-
-def get_order_from_db(order_id: str) -> OrderValue | None:
+async def get_order_from_db(order_id: str) -> OrderValue | None:
     try:
         # get serialized data
-        entry: bytes = db.get(order_id)
+        entry: bytes = await db.get(order_id)
     except redis.exceptions.RedisError:
         return abort(400, DB_ERROR_STR)
     # deserialize data if it exists else return null
@@ -52,20 +54,23 @@ def get_order_from_db(order_id: str) -> OrderValue | None:
         abort(400, f"Order: {order_id} not found!")
     return entry
 
+@app.before_serving
+async def startup():
+    await rabbit_client.start()
 
 @app.post('/create/<user_id>')
-def create_order(user_id: str):
+async def create_order(user_id: str):
     key = str(uuid.uuid4())
     value = msgpack.encode(OrderValue(paid=False, items=[], user_id=user_id, total_cost=0))
     try:
-        db.set(key, value)
+        await db.set(key, value)
     except redis.exceptions.RedisError:
         return abort(400, DB_ERROR_STR)
     return jsonify({'order_id': key})
 
 
 @app.post('/batch_init/<n>/<n_items>/<n_users>/<item_price>')
-def batch_init_users(n: int, n_items: int, n_users: int, item_price: int):
+async def batch_init_users(n: int, n_items: int, n_users: int, item_price: int):
 
     n = int(n)
     n_items = int(n_items)
@@ -85,15 +90,15 @@ def batch_init_users(n: int, n_items: int, n_users: int, item_price: int):
     kv_pairs: dict[str, bytes] = {f"{i}": msgpack.encode(generate_entry())
                                   for i in range(n)}
     try:
-        db.mset(kv_pairs)
+        await db.mset(kv_pairs)
     except redis.exceptions.RedisError:
         return abort(400, DB_ERROR_STR)
     return jsonify({"msg": "Batch init for orders successful"})
 
 
 @app.get('/find/<order_id>')
-def find_order(order_id: str):
-    order_entry: OrderValue = get_order_from_db(order_id)
+async def find_order(order_id: str):
+    order_entry: OrderValue = await get_order_from_db(order_id)
     return jsonify(
         {
             "order_id": order_id,
@@ -105,7 +110,7 @@ def find_order(order_id: str):
     )
 
 
-def send_post_request(url: str):
+async def send_post_request(url: str):
     try:
         response = requests.post(url)
     except requests.exceptions.RequestException:
@@ -114,7 +119,7 @@ def send_post_request(url: str):
         return response
 
 
-def send_get_request(url: str):
+async def send_get_request(url: str):
     try:
         response = requests.get(url)
     except requests.exceptions.RequestException:
@@ -124,9 +129,9 @@ def send_get_request(url: str):
 
 
 @app.post('/addItem/<order_id>/<item_id>/<quantity>')
-def add_item(order_id: str, item_id: str, quantity: int):
-    order_entry: OrderValue = get_order_from_db(order_id)
-    item_reply = send_get_request(f"{GATEWAY_URL}/stock/find/{item_id}")
+async def add_item(order_id: str, item_id: str, quantity: int):
+    order_entry: OrderValue = await get_order_from_db(order_id)
+    item_reply = await send_get_request(f"{GATEWAY_URL}/stock/find/{item_id}")
     if item_reply.status_code != 200:
         # Request failed because item does not exist
         abort(400, f"Item: {item_id} does not exist!")
@@ -134,22 +139,22 @@ def add_item(order_id: str, item_id: str, quantity: int):
     order_entry.items.append((item_id, int(quantity)))
     order_entry.total_cost += int(quantity) * item_json["price"]
     try:
-        db.set(order_id, msgpack.encode(order_entry))
+        await db.set(order_id, msgpack.encode(order_entry))
     except redis.exceptions.RedisError:
         return abort(400, DB_ERROR_STR)
     return Response(f"Item: {item_id} added to: {order_id} price updated to: {order_entry.total_cost}",
                     status=200)
 
 
-def rollback_stock(removed_items: list[tuple[str, int]]):
+async def rollback_stock(removed_items: list[tuple[str, int]]):
     for item_id, quantity in removed_items:
-        send_post_request(f"{GATEWAY_URL}/stock/add/{item_id}/{quantity}")
+        await send_post_request(f"{GATEWAY_URL}/stock/add/{item_id}/{quantity}")
 
 
 @app.post('/checkout/<order_id>')
-def checkout(order_id: str):
+async def checkout(order_id: str):
     app.logger.debug(f"Checking out {order_id}")
-    order_entry: OrderValue = get_order_from_db(order_id)
+    order_entry: OrderValue = await get_order_from_db(order_id)
     # get the quantity per item
     items_quantities: dict[str, int] = defaultdict(int)
     for item_id, quantity in order_entry.items:
@@ -158,20 +163,20 @@ def checkout(order_id: str):
     # for rollback purposes.
     removed_items: list[tuple[str, int]] = []
     for item_id, quantity in items_quantities.items():
-        stock_reply = send_post_request(f"{GATEWAY_URL}/stock/subtract/{item_id}/{quantity}")
+        stock_reply = await send_post_request(f"{GATEWAY_URL}/stock/subtract/{item_id}/{quantity}")
         if stock_reply.status_code != 200:
             # If one item does not have enough stock we need to rollback
-            rollback_stock(removed_items)
+            await rollback_stock(removed_items)
             abort(400, f'Out of stock on item_id: {item_id}')
         removed_items.append((item_id, quantity))
-    user_reply = send_post_request(f"{GATEWAY_URL}/payment/pay/{order_entry.user_id}/{order_entry.total_cost}")
+    user_reply = await send_post_request(f"{GATEWAY_URL}/payment/pay/{order_entry.user_id}/{order_entry.total_cost}")
     if user_reply.status_code != 200:
         # If the user does not have enough credit we need to rollback all the item stock subtractions
-        rollback_stock(removed_items)
+        await rollback_stock(removed_items)
         abort(400, "User out of credit")
     order_entry.paid = True
     try:
-        db.set(order_id, msgpack.encode(order_entry))
+        await db.set(order_id, msgpack.encode(order_entry))
     except redis.exceptions.RedisError:
         return abort(400, DB_ERROR_STR)
     app.logger.debug("Checkout successful")
