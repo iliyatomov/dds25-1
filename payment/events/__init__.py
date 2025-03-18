13066dc8766949ca8b5856723775079553077d41
iliyatomov <i.tomov@fdcl.nl>
2025-03-18 19:35:10 +0100

    fix config and file layout

---
+++ payment/events/__init__.py
@@ -0,0 +1,6 @@
+from .order_placed import *
+from .order_cancelled import *
+from .stock_reserved import *
+from .insufficient_stock import *
+from .order_paid import *
+from .order_placed import *
---
+++ payment/events/insufficient_stock.py
@@ -0,0 +1,8 @@
+from msgspec import Struct
+
+class InsufficientStockEvent(Struct):
+    order_id: str
+    user_id: str
+    items: list[tuple[str, int]]
+    total_cost: int
+    order_handling_service_id: str
