4dd99ac5a1e968045270cf7eebad87fa022d6aa3
iliyatomov <i.tomov@fdcl.nl>
2025-04-09 20:03:53 +0200

    Refactor endpoints

--- stock/app.py
+++ stock/app.py
@@ -169,104 +169,103 @@ async def shutdown():
 async def on_order_paid(message: IncomingMessage):
     event = msgpack.decode(message.body, type=OrderPaidEvent)
 
-    order_id = event.order_id
-    user_id = str(event.user_id)
-    total_cost = event.total_cost
-    items = event.items
+    async with database.transaction():
+        order_id = event.order_id
+        user_id = str(event.user_id)
+        total_cost = event.total_cost
+        items = event.items
 
-    response = 0
-    item_ids = [item[0] for item in items]
-    quantity_map = {item[0]: item[1] for item in items}
+        response = 0
+        item_ids = [item[0] for item in items]
+        quantity_map = {item[0]: item[1] for item in items}
 
-    query = items_table.select().where(items_table.c.item_id.in_(item_ids))
-    rows = await database.fetch_all(query=query)
+        query = items_table.select().where(items_table.c.item_id.in_(item_ids))
+        rows = await database.fetch_all(query=query)
 
-    if len(rows) != len(item_ids):
-        response = -1
+        if len(rows) != len(item_ids):
+            response = -1
 
-    if response == 0:
-        insufficient = False
-        new_stocks = {}
+        if response == 0:
+            insufficient = False
+            new_stocks = {}
 
-        for row in rows:
-            item_id = row['item_id']
-            current_stock = row['stock']
-            quantity_needed = quantity_map[item_id]
+            for row in rows:
+                item_id = row['item_id']
+                current_stock = row['stock']
+                quantity_needed = quantity_map[item_id]
 
-            new_stock = current_stock - quantity_needed
-            if new_stock < 0:
-                insufficient = True
-                break
-            new_stocks[item_id] = new_stock
+                new_stock = current_stock - quantity_needed
+                if new_stock < 0:
+                    insufficient = True
+                    break
+                new_stocks[item_id] = new_stock
 
 
-        if insufficient:
-            response = -2
+            if insufficient:
+                response = -2
 
-    if response == 0:
-        async with database.transaction():
-            for item_id, new_stock in new_stocks.items():
-                update_query = (
-                    items_table.update()
-                    .where(items_table.c.item_id == item_id)
-                    .values(stock=new_stock)
+        if response == 0:
+                for item_id, new_stock in new_stocks.items():
+                    update_query = (
+                        items_table.update()
+                        .where(items_table.c.item_id == item_id)
+                        .values(stock=new_stock)
+                    )
+                    await database.execute(update_query)
+
+                stock_reserved_event = StockReservedEvent(
+                    order_id=order_id,
+                    user_id=user_id,
+                    items=items,
+                    total_cost=total_cost,
+                    order_handling_service_id=event.order_handling_service_id
                 )
-                await database.execute(update_query)
-
-            stock_reserved_event = StockReservedEvent(
-                order_id=order_id,
-                user_id=user_id,
-                items=items,
-                total_cost=total_cost,
-                order_handling_service_id=event.order_handling_service_id
-            )
-            stmt = select(outbox_table).where(
-                and_(
-                    outbox_table.c.aggregateid == order_id,
-                    outbox_table.c.type == "stock-reserved"
+                stmt = select(outbox_table).where(
+                    and_(
+                        outbox_table.c.aggregateid == order_id,
+                        outbox_table.c.type == "stock-reserved"
+                    )
                 )
-            )
-            result = await database.fetch_one(stmt)
+                result = await database.fetch_one(stmt)
 
-            if not result:
-                insert_event = outbox_table.insert().values(
-                    id=uuid.uuid4(),
-                    aggregatetype="stock",
-                    aggregateid=order_id,
-                    type="stock-reserved",
-                    payload=msgspec.to_builtins(stock_reserved_event)
+                if not result:
+                    insert_event = outbox_table.insert().values(
+                        id=uuid.uuid4(),
+                        aggregatetype="stock",
+                        aggregateid=order_id,
+                        type="stock-reserved",
+                        payload=msgspec.to_builtins(stock_reserved_event)
+                    )
+                    await database.execute(insert_event)
+
+        else:
+                insufficient_stock_event = InsufficientStockEvent(
+                    order_id=order_id,
+                    user_id=user_id,
+                    items=items,
+                    total_cost=total_cost,
+                    order_handling_service_id=event.order_handling_service_id
                 )
-                await database.execute(insert_event)
 
-    else:
-        async with database.transaction():
-            insufficient_stock_event = InsufficientStockEvent(
-                order_id=order_id,
-                user_id=user_id,
-                items=items,
-                total_cost=total_cost,
-                order_handling_service_id=event.order_handling_service_id
-            )
-
-            stmt = select(outbox_table).where(
-                and_(
-                    outbox_table.c.aggregateid == order_id,
-                    outbox_table.c.type == "insufficient-stock"
+                stmt = select(outbox_table).where(
+                    and_(
+                        outbox_table.c.aggregateid == order_id,
+                        outbox_table.c.type == "insufficient-stock"
+                    )
                 )
-            )
-            result = await database.fetch_one(stmt)
+                result = await database.fetch_one(stmt)
 
-            if not result:
-                insert_event = outbox_table.insert().values(
-                    id=uuid.uuid4(),
-                    aggregatetype="stock",
-                    aggregateid=order_id,
-                    type="insufficient-stock",
-                    payload=msgspec.to_builtins(insufficient_stock_event)
-                )
-                await database.execute(insert_event)
+                if not result:
+                    insert_event = outbox_table.insert().values(
+                        id=uuid.uuid4(),
+                        aggregatetype="stock",
+                        aggregateid=order_id,
+                        type="insufficient-stock",
+                        payload=msgspec.to_builtins(insufficient_stock_event)
+                    )
+                    await database.execute(insert_event)
 
-    await message.ack()
+        await message.ack()
 
 
 @app.post('/item/create/<price>')
@@ -274,24 +273,36 @@ async def create_item(price: int):
     key = str(uuid.uuid4())
     app.logger.debug(f"Item: {key} created")
     value = msgpack.encode(StockValue(stock=0, price=int(price)))
-    # try:
-    #     await db.set(key, value)
-    # except redis.exceptions.RedisError:
-    #     return abort(400, DB_ERROR_STR)
+
+    item_data = msgpack.decode(value)
+
+    stmt = items_table.insert().values(
+        item_id=key,
+        stock=item_data['stock'],
+        price=item_data['price']
+    )
+
+    try:
+        await database.execute(stmt)
+    except Exception as e:
+        app.logger.error(f"Error creating item: {e}")
+        return abort(400, DB_ERROR_STR)
+
     return jsonify({'item_id': key})
 
 
 @app.post('/batch_init/<n>/<starting_stock>/<item_price>')
 async def batch_init_users(n: int, starting_stock: int, item_price: int):
-    n = int(n)
-    starting_stock = int(starting_stock)
-    item_price = int(item_price)
-    kv_pairs: dict[str, bytes] = {f"{i}": msgpack.encode(StockValue(stock=starting_stock, price=item_price))
-                                  for i in range(n)}
-    # try:
-    #     await db.mset(kv_pairs)
-    # except redis.exceptions.RedisError:
-    #     return abort(400, DB_ERROR_STR)
+    try:
+        item_data = [{"item_id": str(i), "stock": starting_stock, "price": item_price} for i in range(n)]
+
+        stmt = items_table.insert().values(item_data)
+        await database.execute(stmt)
+        await database.commit()
+    except Exception as e:
+        app.logger.error(f"Error creating items: {e}")
+        return abort(400, DB_ERROR_STR)
+
     return jsonify({"msg": "Batch init for stock successful"})
 
 
@@ -311,10 +322,18 @@ async def add_stock(item_id: str, amount: int):
     item_entry: StockValue = await get_item_from_db(item_id)
     # update stock, serialize and update database
     item_entry.stock += int(amount)
-    # try:
-    #     await db.set(item_id, msgpack.encode(item_entry))
-    # except redis.exceptions.RedisError:
-    #     return abort(400, DB_ERROR_STR)
+
+    try:
+        update_query = (
+            items_table.update()
+            .where(items_table.c.item_id == item_id)
+            .values(stock=item_entry.stock)
+        )
+        await database.execute(update_query)
+    except Exception as e:
+        app.logger.error(f"Error updating item: {e}")
+        return abort(400, DB_ERROR_STR)
+
     return Response(f"Item: {item_id} stock updated to: {item_entry.stock}", status=200)
 
 
@@ -326,10 +345,18 @@ async def remove_stock(item_id: str, amount: int):
     app.logger.debug(f"Item: {item_id} stock updated to: {item_entry.stock}")
     if item_entry.stock < 0:
         abort(400, f"Item: {item_id} stock cannot get reduced below zero!")
-    # try:
-    #     await db.set(item_id, msgpack.encode(item_entry))
-    # except redis.exceptions.RedisError:
-    #     return abort(400, DB_ERROR_STR)
+
+    try:
+        update_query = (
+            items_table.update()
+            .where(items_table.c.item_id == item_id)
+            .values(stock=item_entry.stock)
+        )
+        await database.execute(update_query)
+    except Exception as e:
+        app.logger.error(f"Error updating item: {e}")
+        return abort(400, DB_ERROR_STR)
+
     return Response(f"Item: {item_id} stock updated to: {item_entry.stock}", status=200)
 
 
